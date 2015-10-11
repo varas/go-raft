b34dad4dcff72d9e2272b922c5efc9606a8de02f
jhvaras <jhvaras@gmail.com>
2015-10-11 22:08:07 +0200

    fix print

--- node_fsm_test.go
+++ node_fsm_test.go
@@ -2,10 +2,15 @@ package raft
 
 import (
     "testing"
+    "fmt"
 )
 
 func TestAllNodesStartAsFollowers(t *testing.T){
     cluster := NewCluster(10)
-    nodes := cluster.Nodes
-    println(nodes)
+    nodes := cluster.Nodes()
+
+    fmt.Println("nodes: ", nodes)
+    for k, v := range nodes {
+        fmt.Println(k, ": ", v)
+    }
 }
